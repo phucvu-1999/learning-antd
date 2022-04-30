1680132d264ab41370b38527d2aadc898ffe9ff7
Tien Phuc <leesinvn123@gmail.com>
2022-04-30 23:23:19 +0700

    Learning DatePicker antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
 import React, { useState } from "react";
-import { Form, Input, Button, message, Alert } from "antd";
+import { DatePicker } from "antd";
 
 import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
-  const [form] = Form.useForm();
-  const [showAlert, setShowAlert] = useState(false);
-
-  const onFinish = () => {
-    console.log(form.getFieldsValue());
-    setTimeout(() => {
-      setShowAlert(true);
-    }, 2000);
+  const onMonthChange = (e: moment.Moment | null) => {
+    console.log(e?.format("L"));
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        {showAlert && (
-          <Alert
-            type="error"
-            description="Error login"
-            message="Some thing went wrong"
-          />
-        )}
-        <Form form={form} layout="vertical" onFinish={onFinish} id="loginForm">
-          <Form.Item name="username" label="Username">
-            <Input placeholder="Please enter your username" />
-          </Form.Item>
-
-          <Form.Item name="password" label="Password">
-            <Input.Password placeholder="Please enter your password" />
-          </Form.Item>
-
-          <Form.Item>
-            <Button htmlType="submit" form="loginForm">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        <DatePicker picker="month" format="MM/YYYY" onChange={onMonthChange} />
       </header>
     </div>
   );
