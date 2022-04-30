30289193202966c283c4d1dcbaa740acb4afe082
Tien Phuc <leesinvn123@gmail.com>
2022-04-30 23:11:30 +0700

    Learning Alert and message in antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, message, Alert } from "antd";
 
 import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
   const [form] = Form.useForm();
+  const [showAlert, setShowAlert] = useState(false);
 
   const onFinish = () => {
     console.log(form.getFieldsValue());
+    setTimeout(() => {
+      setShowAlert(true);
+    }, 2000);
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        {showAlert && (
+          <Alert
+            type="error"
+            description="Error login"
+            message="Some thing went wrong"
+          />
+        )}
         <Form form={form} layout="vertical" onFinish={onFinish} id="loginForm">
           <Form.Item name="username" label="Username">
             <Input placeholder="Please enter your username" />
@@ -23,8 +34,10 @@ function App() {
             <Input.Password placeholder="Please enter your password" />
           </Form.Item>
 
-          <Form.Item name="submitBtn" label="Submit">
-            <Button htmlType="submit" form="loginForm"></Button>
+          <Form.Item>
+            <Button htmlType="submit" form="loginForm">
+              Submit
+            </Button>
           </Form.Item>
         </Form>
       </header>
