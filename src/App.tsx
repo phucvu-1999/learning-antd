bc7f4a86d09733e65fc461d1feda0edbb77dc133
Tien Phuc <leesinvn123@gmail.com>
2022-04-30 21:49:51 +0700

    Learning antd button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from "react";
-import { Table, Button } from "antd";
+import React, { useState } from "react";
+import { Button } from "antd";
+import { PoweroffOutlined } from "@ant-design/icons";
 
 import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  const onBtnClick = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button type="primary">Add new </Button>
+        <Button
+          type="primary"
+          icon={<PoweroffOutlined />}
+          block
+          onClick={onBtnClick}
+          loading={loading}
+        >
+          Add new
+        </Button>
       </header>
     </div>
   );
